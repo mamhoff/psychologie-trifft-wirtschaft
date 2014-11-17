2bfe0c467f901211d3162108f3f311aa46079151
Martin Meyerhoff <mamhoff@googlemail.com>
2014-11-17 19:30:43 +0100

    rudimentary fullpaging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,22 @@
 //= require jquery
 //= require bootstrap-sprockets
+
+var showErrorModals = function() {
+  $('.has-error').parents('.modal').modal('show')
+};
+
+var resizeSections = function(){
+    var windowHeight = $(window).height() - $('.navbar').first().height();
+    $.each($('.section'), function(i, e) {
+      $(e).css('min-height', windowHeight + 'px');
+    });
+};
+
+var startupFunctions = function() {
+  showErrorModals();
+  resizeSections();
+};
+
+$(document).ready(startupFunctions);
+$(document).ajaxStop(startupFunctions);
+$(window).resize(resizeSections);
