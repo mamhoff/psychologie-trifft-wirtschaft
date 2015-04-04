a1e466ff820dc32ceb38edd1b61885336152b807
Martin Meyerhoff <mamhoff@googlemail.com>
2015-04-04 14:02:58 +0200

    remove scrolling crazyness

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,28 +1,2 @@
 //= require jquery
 //= require bootstrap-sprockets
-//= require jquery.scrollTo
-//= require jquery-localscroll
-//= require jquery-parallax
-
-
-var showErrorModals = function() {
-  $('.has-error').parents('.modal').modal('show')
-};
-
-var resizeSections = function(){
-    var windowHeight = $(window).height() - $('.navbar').first().height();
-    $.each($('.section'), function(i, e) {
-      $(e).css('min-height', windowHeight + 'px');
-    });
-};
-
-var startupFunctions = function() {
-  showErrorModals();
-  resizeSections();
-  $.localScroll({offset: {top: -60}});
-};
-
-$(document).ready(startupFunctions);
-$(document).ajaxStop(startupFunctions);
-$(window).resize(resizeSections);
-
