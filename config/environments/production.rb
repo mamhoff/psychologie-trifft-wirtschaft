e73a6bbe9ac6ca753fef88ec18090478b03655f3
Martin Meyerhoff <mamhoff@googlemail.com>
2015-05-11 00:01:32 +0200

    configure sending mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'horologium.uberspace.de',
+    port:                 587,
+    domain:               'psychologie-trifft-wirtschaft.de',
+    user_name:            Rails.application.secrets.email_user,
+    password:             Rails.application.secrets.email_pass,
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
