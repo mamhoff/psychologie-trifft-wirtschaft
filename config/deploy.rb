51bb9303847864e308e41f75156be8340d3ed73e
Martin Meyerhoff <mamhoff@gmail.com>
2019-05-29 19:01:41 +0200

    Update Capistrano-Uberspace configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.11.0'
 
 set :application, 'ptw'
 set :repo_url, "git@github.com:mamhoff/psychologie-trifft-wirtschaft.git"
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:mamhoff/psychologie-trifft-wirtschaft.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/var/www/virtual/htholen/rails/ptw"
 
 # Default value for :scm is :git
 # set :scm, :git
