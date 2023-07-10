e88e9850523d4dfbfad00c159e6cae5820d648d1
Martin Meyerhoff <mamhoff@gmail.com>
2023-07-10 16:02:28 +0200

    Alchemy 5.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_10_105222) do
+ActiveRecord::Schema.define(version: 2023_07_10_135812) do
 
   create_table "alchemy_attachments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
@@ -51,9 +51,9 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.string "name"
     t.integer "position"
     t.integer "page_id", null: false
-    t.boolean "public", default: true
-    t.boolean "folded", default: false
-    t.boolean "unique", default: false
+    t.boolean "public", default: true, null: false
+    t.boolean "folded", default: false, null: false
+    t.boolean "unique", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.integer "updater_id"
   end
 
-  create_table "alchemy_essence_nodes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci", force: :cascade do |t|
+  create_table "alchemy_essence_nodes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "node_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -157,7 +157,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
   create_table "alchemy_essence_richtexts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.text "body"
     t.text "stripped_body"
-    t.boolean "public"
+    t.boolean "public", default: false, null: false
     t.integer "creator_id"
     t.integer "updater_id"
     t.datetime "created_at", null: false
@@ -178,7 +178,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.string "link"
     t.string "link_title"
     t.string "link_class_name"
-    t.boolean "public", default: false
+    t.boolean "public", default: false, null: false
     t.string "link_target"
     t.integer "creator_id"
     t.integer "updater_id"
@@ -189,7 +189,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
   create_table "alchemy_folded_pages", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "page_id", null: false
     t.integer "user_id", null: false
-    t.boolean "folded", default: false
+    t.boolean "folded", default: false, null: false
     t.index ["page_id", "user_id"], name: "index_alchemy_folded_pages_on_page_id_and_user_id", unique: true
   end
 
@@ -198,12 +198,12 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.string "language_code"
     t.string "frontpage_name"
     t.string "page_layout", default: "intro"
-    t.boolean "public", default: false
+    t.boolean "public", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
     t.integer "updater_id"
-    t.boolean "default", default: false
+    t.boolean "default", default: false, null: false
     t.string "country_code", default: "", null: false
     t.integer "site_id", null: false
     t.string "locale"
@@ -253,7 +253,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.string "urlname"
     t.string "title"
     t.string "language_code"
-    t.boolean "language_root"
+    t.boolean "language_root", default: false, null: false
     t.string "page_layout"
     t.text "meta_keywords"
     t.text "meta_description"
@@ -262,10 +262,10 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.integer "parent_id"
     t.integer "depth"
     t.integer "locked_by"
-    t.boolean "restricted", default: false
-    t.boolean "robot_index", default: true
-    t.boolean "robot_follow", default: true
-    t.boolean "sitemap", default: true
+    t.boolean "restricted", default: false, null: false
+    t.boolean "robot_index", default: true, null: false
+    t.boolean "robot_follow", default: true, null: false
+    t.boolean "sitemap", default: true, null: false
     t.boolean "layoutpage", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -282,6 +282,14 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.index ["public_on", "public_until"], name: "index_alchemy_pages_on_public_on_and_public_until"
     t.index ["rgt"], name: "index_alchemy_pages_on_rgt"
     t.index ["urlname"], name: "index_pages_on_urlname"
+  end
+
+  create_table "alchemy_picture_thumbs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci", force: :cascade do |t|
+    t.integer "picture_id", null: false
+    t.string "signature", null: false
+    t.text "uid", null: false
+    t.index ["picture_id"], name: "index_alchemy_picture_thumbs_on_picture_id"
+    t.index ["signature"], name: "index_alchemy_picture_thumbs_on_signature", unique: true
   end
 
   create_table "alchemy_pictures", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
@@ -304,9 +312,9 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "public", default: false
+    t.boolean "public", default: false, null: false
     t.text "aliases"
-    t.boolean "redirect_to_primary_host"
+    t.boolean "redirect_to_primary_host", default: false, null: false
     t.index ["host", "public"], name: "alchemy_sites_public_hosts_idx"
     t.index ["host"], name: "index_alchemy_sites_on_host"
   end
@@ -371,4 +379,5 @@ ActiveRecord::Schema.define(version: 2023_07_10_105222) do
   add_foreign_key "alchemy_nodes", "alchemy_languages", column: "language_id"
   add_foreign_key "alchemy_nodes", "alchemy_pages", column: "page_id", on_delete: :cascade
   add_foreign_key "alchemy_pages", "alchemy_languages", column: "language_id"
+  add_foreign_key "alchemy_picture_thumbs", "alchemy_pictures", column: "picture_id"
 end
